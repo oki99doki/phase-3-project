8bad82421a478bd89fbfdea1959843110872156e
Jason Malefakis <anskyny10@aol.com>
2024-08-19 14:56:38 -0400

    super initial setup

---
+++ lib/models/game.py
@@ -0,0 +1,25 @@
+import sqlite3 
+
+CONN = sqlite3.connect('resources.db', timeout=10) 
+CURSOR = CONN.cursor()
+ 
+class Game:
+
+    def __init__(self, outcome, user_id = None, id=None):
+        self.outcome = outcome
+        self.user_id = user_id
+        self.id = id
+
+    @classmethod 
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS games
+                (
+                    id INTEGER PRIMARY KEY,
+                    outcome TEXT,
+                    user_id INTEGER,
+                    FOREIGN KEY (user_id) REFERENCES users(id)
+                );
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
