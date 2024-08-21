c5599c74013de1bc01c0f8c9252d3830d816578f
Jason Malefakis <anskyny10@aol.com>
2024-08-21 19:11:38 -0400

    add CLI option template to fulfill requirement

--- lib/cli.py
+++ lib/cli.py
@@ -28,17 +28,19 @@ def main():
         elif choice == "2":
             play_game()
         elif choice == "3":
-            Game.view_results()
+            Game.view_results()   # display all objects/results of games requirement
         elif choice == "4":
-            add_new_question()
+            add_new_question()    # create object requirement
         elif choice == "5":
             User.delete() #(need to include the current user id)
         elif choice == "6":
-            Question.drop_table()
+            Question.drop_table()    # delete object requirement
         elif choice == "7":
             Game.drop_table()
         elif choice == "8":
             Game.find_by_id()  # <-- WE NEED TO ADD FIND OBJECT BY ID/ATTRIBUTE
+        elif choice == "9":
+            Game.view_my_games()  # <-- WE NEED TO ADD VIEW RELATED OBJECTS FOR THE CURRENT USER
         else:
             print("Invalid choice")
             
@@ -55,6 +57,7 @@ def menu():
     print("5. Delete current user")
     print("6. Delete all questions")
     print("7. Delete all games")
+    print("8. Find user by id")
 
 
 if __name__ == "__main__":
